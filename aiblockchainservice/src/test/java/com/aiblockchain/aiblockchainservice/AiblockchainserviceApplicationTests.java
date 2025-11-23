f4a7d13229fd7a968bbd9bd43b5847874645b7e3
RYUJEONGHUN <ryugh0210@naver.com>
2025-11-23 11:04:51 +0900

    테스트

---
+++ aiblockchainservice/src/test/java/com/aiblockchain/aiblockchainservice/AiblockchainserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aiblockchain.aiblockchainservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiblockchainserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
