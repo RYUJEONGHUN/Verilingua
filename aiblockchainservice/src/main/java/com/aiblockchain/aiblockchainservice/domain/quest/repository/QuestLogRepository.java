0727f2999000d0b4678e5576f9aec7dfcd2c333f
RYUJEONGHUN <ryugh0210@naver.com>
2025-11-25 22:41:00 +0900

    퀘스트 로그 레포지토리

--- aiblockchainservice/src/main/java/com/aiblockchain/aiblockchainservice/domain/quest/repository/QuestLogRepository.java
+++ aiblockchainservice/src/main/java/com/aiblockchain/aiblockchainservice/domain/quest/repository/QuestLogRepository.java
@@ -1,8 +1,9 @@
 package com.aiblockchain.aiblockchainservice.domain.quest.repository;
 
 import com.aiblockchain.aiblockchainservice.domain.quest.entity.Quest;
+import com.aiblockchain.aiblockchainservice.domain.quest.entity.QuestLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface QuestLogRepository extends JpaRepository<Quest, Long> {
+public interface QuestLogRepository extends JpaRepository<QuestLog, Long> {
 
 }
