c45845d131ca8cee23d3306692268abc36f3ed9a
RYUJEONGHUN <ryugh0210@naver.com>
2025-11-30 13:43:24 +0900

    프론트 에러처리를 위함

--- aiblockchainservice/src/main/java/com/aiblockchain/aiblockchainservice/global/config/SecurityConfig.java
+++ aiblockchainservice/src/main/java/com/aiblockchain/aiblockchainservice/global/config/SecurityConfig.java
@@ -7,18 +7,20 @@ import com.aiblockchain.aiblockchainservice.global.jwt.JWTUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-
+    private final CorsConfig corsConfig; // 👈 주입 받기
     private final CustomOAuth2UserService customOAuth2UserService;
     private final OAuth2SuccessHandler oAuth2SuccessHandler;
     private final JWTUtil jwtUtil;
@@ -27,12 +29,17 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .addFilter(corsConfig.corsFilter())
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
 
-                // 🚀 JWT를 쓰므로 세션을 STATELESS로 설정
+                //  JWT를 쓰므로 세션을 STATELESS로 설정
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
+                // 🚀 [추가] 인증 안 된 요청은 그냥 401을 내려라! (로그인 페이지로 보내지 말고)
+                .exceptionHandling(e -> e
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                )
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/", "/login/**", "/oauth2/**", "/login-success").permitAll()
                         .anyRequest().authenticated()
@@ -43,7 +50,7 @@ public class SecurityConfig {
                         .successHandler(oAuth2SuccessHandler)
                 )
 
-                // 🚀 JWT 필터 추가 (로그인 필터 앞단에 배치)
+                //  JWT 필터 추가 (로그인 필터 앞단에 배치)
                 .addFilterBefore(new JWTFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
